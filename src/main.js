570b72aebe9a7762a12bc78d709dd2151892c08a
vicpsl <jvboby@gmail.com>
2026-09-13 17:59:10 +0200

    refactor(main.js): remove unused code

--- src/main.js
+++ src/main.js
@@ -16,22 +16,3 @@ const app = createApp(App).provide('variable-layout', useLayout())
   .use(router)
   .use(store)
 app.mount('#app')
-/* router.isReady().then(() => {
-  app.mount('#app')
-}) */
-
-/* const modules = import.meta.glob('@/views/about/*.vue', { eager: true })
-
-export default function loadComponents (app) {
-  for (const path in modules) {
-    const componentName = path.split('/').at(-1).split('.')[0]
-    app.component(`Gen${componentName}`, modules[path].default)
-  }
-} */
-
-/* const app = createApp(App).provide("variable-layout", useLayout())
-
-app.use(router)
-app.use(store)
-
-app.mount('#app') */
